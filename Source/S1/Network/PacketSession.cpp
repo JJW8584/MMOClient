1d5727ffc55e3e8eec249bff8f4855d9e50907e9
JJW <jjw8584@gmail.com>
2025-08-07 21:39:30 +0900

    Network Setting #1

---
+++ Source/S1/Network/PacketSession.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Network/PacketSession.h"
+
+PacketSession::PacketSession(FSocket* Socket) : Socket(Socket)
+{
+}
+
+PacketSession::~PacketSession()
+{
+	Disconnect();
+}
+
+void PacketSession::Run()
+{
+
+}
+
+void PacketSession::Disconnect()
+{
+
+}

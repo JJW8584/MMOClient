3d3a75966c468981828249fd77a43df2982be5b4
JJW <jjw8584@gmail.com>
2025-09-03 15:54:48 +0900

    Despawn

--- Source/S1/S1GameInstance.cpp
+++ Source/S1/S1GameInstance.cpp
@@ -49,12 +49,18 @@ void US1GameInstance::ConnectToGameServer()
 
 void US1GameInstance::DisconnectFromGameServer()
 {
-	if (Socket)
+	if (Socket == nullptr || GameServerSession == nullptr)
+		return;
+
+	Protocol::C_LEAVE_GAME LeavePkt;
+	SEND_PACKET(LeavePkt);
+
+	/*if (Socket)
 	{
 		ISocketSubsystem* SocketSubsystem = ISocketSubsystem::Get();
 		SocketSubsystem->DestroySocket(Socket);
 		Socket = nullptr;
-	}
+	}*/
 }
 
 void US1GameInstance::HandleRecvPackets()
@@ -103,5 +109,29 @@ void US1GameInstance::HandleSpawn(const Protocol::S_SPAWN& SpawnPkt)
 	for (auto& Player : SpawnPkt.players())
 	{
 		HandleSpawn(Player);
+	}
+}
+
+void US1GameInstance::HandleDespawn(uint64 ObjectId)
+{
+	if (Socket == nullptr || GameServerSession == nullptr)
+		return;
+
+	auto* World = GetWorld();
+	if (World == nullptr)
+		return;
+
+	AActor** FindActor = Players.Find(ObjectId);
+	if (FindActor == nullptr)
+		return;
+
+	World->DestroyActor(*FindActor);
+}
+
+void US1GameInstance::HandleDespawn(const Protocol::S_DESPAWN& DespawnPkt)
+{
+	for (auto& ObjectId : DespawnPkt.object_ids())
+	{
+		HandleDespawn(ObjectId);
 	}
 }

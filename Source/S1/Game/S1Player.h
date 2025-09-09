f51cd2ddd35890ed2934f1d9203882efce4370ef
JJW <90325769+JJW8584@users.noreply.github.com>
2025-09-09 22:35:05 +0900

    Move Correction

--- Source/S1/Game/S1Player.h
+++ Source/S1/Game/S1Player.h
@@ -18,16 +18,22 @@ public:
 	~AS1Player();
 
 protected:
+	virtual void BeginPlay();
 	virtual void Tick(float DeltaSeconds) override;
 
 public:
 	bool IsMyPlayer();
 
+	Protocol::MoveState GetMoveState() { return PlayerInfo->state(); }
+	void SetMoveState(Protocol::MoveState State);
+
 public:
 	void SetPlayerInfo(const Protocol::PlayerInfo& Info);
+	void SetDestInfo(const Protocol::PlayerInfo& Info);
 	Protocol::PlayerInfo* GetPlayerInfo() { return PlayerInfo; }
 
 
 protected:
 	class Protocol::PlayerInfo* PlayerInfo; // 현재 위치
+	class Protocol::PlayerInfo* DestInfo; // 목적지
 };
